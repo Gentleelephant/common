698b8c1b10a723ec9948b552462848e35c9606fc
zhangpeng <1132960613@qq.com>
2022-07-30 17:08:18 +0800

    init db and service register

--- utils/init_db.go
+++ utils/init_db.go
@@ -1,3 +1,45 @@
 package utils
 
-// get gorm.DB from
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+	"log"
+	"os"
+	"time"
+)
+
+type SQLConfig struct {
+	Host     string `json:"host"`
+	Port     int    `json:"port"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Database string `json:"database"`
+}
+
+func InitDB(config SQLConfig) (*gorm.DB, error) {
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer（日志输出的目标，前缀和日志包含的内容——译者注）
+		logger.Config{
+			SlowThreshold:             time.Second,   // 慢 SQL 阈值
+			LogLevel:                  logger.Silent, // 日志级别
+			IgnoreRecordNotFoundError: true,          // 忽略ErrRecordNotFound（记录未找到）错误
+			Colorful:                  false,         // 禁用彩色打印
+		},
+	)
+
+	// 从配置中读取数据库配置
+	conn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", config.Username, config.Password, config.Host, config.Port, config.Database)
+	open, err := gorm.Open(mysql.Open(conn), &gorm.Config{
+		Logger: newLogger,
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true, // 是否使用单数表名
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	return open, nil
+}
--- utils/service_register.go
+++ utils/service_register.go
@@ -1,3 +1,77 @@
 package utils
 
-// service register
+//
+//import (
+//	"context"
+//	"encoding/json"
+//	"fmt"
+//	clientv3 "go.etcd.io/etcd/client/v3"
+//	"go.uber.org/zap"
+//	"os"
+//	"time"
+//)
+//
+//// service register
+//package internal
+//
+//import (
+//	"context"
+//	"encoding/json"
+//	"fmt"
+//	"github.com/Gentleelephant/account-srv/config"
+//	clientv3 "go.etcd.io/etcd/client/v3"
+//	"go.uber.org/zap"
+//	"os"
+//	"time"
+//)
+//
+//var (
+//	// 服务入口在 etcd 存储的 key 前缀
+//	serviceEndpointKeyPrefix = "/service_register"
+//	hostname                 string
+//	endpoint                 string
+//)
+//
+//var etcdConfig = clientv3.Config{
+//	Endpoints:            []string{"127.0.0.1:2379"},
+//	DialTimeout:          30 * time.Second,
+//	DialKeepAliveTimeout: 30 * time.Second,
+//}
+//
+//func ServiceRegister(ctx context.Context)  {
+//	hostname, _ = os.Hostname()
+//	cli, err := clientv3.New(etcdConfig)
+//	if err != nil {
+//		panic(err)
+//	}
+//	address := fmt.Sprintf("%s:%d", hostname, config.LocalConfig.GetInt(config.ServiceDynamicPort))
+//	key := fmt.Sprintf("%s/%s/%s", serviceEndpointKeyPrefix, ServiceName, address)
+//	zap.L().Debug("register service", zap.String("service", ServiceName), zap.String("address", address))
+//	// 过期时间: 3秒钟
+//	ttl := 3
+//	// 创建租约
+//	lease, err := cli.Grant(ctx, int64(ttl))
+//	if err != nil {
+//		Logger.Error(err.Error())
+//	}
+//	b, _ := json.Marshal(lease)
+//	zap.L().Info("grant lease success: ", zap.String("", string(b)))
+//	// put kv
+//	res, err := cli.Put(ctx, key, endpoint, clientv3.WithLease(lease.ID))
+//	if err != nil {
+//		zap.L().Error(err.Error())
+//	}
+//	b, _ = json.Marshal(res)
+//	zap.L().Info("put kv with lease success: ", zap.String("", string(b)))
+//	// 保持租约不过期
+//	klRes, err := cli.KeepAlive(ctx, lease.ID)
+//	if err != nil {
+//		panic(err)
+//	}
+//	// 监听续约情况
+//	for v := range klRes {
+//		b, _ = json.Marshal(v)
+//		zap.L().Info("keep lease alive success: ", zap.String("", string(b)))
+//	}
+//	zap.L().Info("keep lease alive end")
+//}
