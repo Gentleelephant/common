6547d05eca672f3589fb39f1a18e27e2fbb462c0
zhangpeng <1132960613@qq.com>
2022-07-31 17:13:10 +0800

    update utils

--- utils/ip_port.go
+++ utils/ip_port.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"errors"
+	"fmt"
 	"net"
 	"strings"
 )
@@ -19,6 +21,7 @@ func GetTCPPort() (int, error) {
 	return port, nil
 }
 
+// GetOutBoundIP 获得可访问internet的ip地址
 func GetOutBoundIP() (ip string, err error) {
 	conn, err := net.Dial("udp", "8.8.8.8:53")
 	if err != nil {
@@ -27,4 +30,28 @@ func GetOutBoundIP() (ip string, err error) {
 	localAddr := conn.LocalAddr().(*net.UDPAddr)
 	ip = strings.Split(localAddr.String(), ":")[0]
 	return
+}
+
+// GetInterfaceIpv4Addr 根据网卡获取本机ip
+func GetInterfaceIpv4Addr(interfaceName string) (addr string, err error) {
+	var (
+		ief      *net.Interface
+		addrs    []net.Addr
+		ipv4Addr net.IP
+	)
+	if ief, err = net.InterfaceByName(interfaceName); err != nil { // get interface
+		return
+	}
+	if addrs, err = ief.Addrs(); err != nil { // get addresses
+		return
+	}
+	for _, addr := range addrs { // get ipv4 address
+		if ipv4Addr = addr.(*net.IPNet).IP.To4(); ipv4Addr != nil {
+			break
+		}
+	}
+	if ipv4Addr == nil {
+		return "", errors.New(fmt.Sprintf("interface %s don't have an ipv4 address\n", interfaceName))
+	}
+	return ipv4Addr.String(), nil
 }
